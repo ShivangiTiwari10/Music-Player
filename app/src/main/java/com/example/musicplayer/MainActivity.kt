cdd7312e88d0be579fcbf19445b4330c35c95d37
Shivangi <shivangit1111@gmail.com>
2023-04-25 16:26:49 +0530

    feat: create media Player

--- app/src/main/java/com/example/musicplayer/MainActivity.kt
+++ app/src/main/java/com/example/musicplayer/MainActivity.kt
@@ -1,16 +1,47 @@
 package com.example.musicplayer
 
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.example.musicplayer.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mediaPlayer: MediaPlayer
+    var totalTime: Int = 0
 
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        supportActionBar?.hide()
+
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        mediaPlayer = MediaPlayer.create(this, R.raw.music)
+        mediaPlayer.setVolume(1f, 1f)
+        totalTime = mediaPlayer.duration
+
+
+        binding.play.setOnClickListener {
+            mediaPlayer.start()
+            Log.d("MusicS", "${mediaPlayer.start()}")
+
+        }
+
+        binding.pause.setOnClickListener {
+            mediaPlayer.pause()
+            Log.d("MusicP", "${mediaPlayer.pause()}")
+
+        }
+        binding.stop.setOnClickListener {
+            mediaPlayer.stop()
+            mediaPlayer.reset()
+            mediaPlayer.release()
+
+            Log.d("MusicStop", "$mediaPlayer")
+        }
     }
+
 }
